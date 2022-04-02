55f1d8e8ca7a4af6346eedf760e3d67c110a46ca
IAmTomaton <mrhomka.com@gmail.com>
2022-04-02 13:09:18 +0500

    Чуть переделал солвер

--- DRQNERSolver.py
+++ DRQNERSolver.py
@@ -1,14 +1,3 @@
-import math
-from time import time
-
-import numpy as np
-
-from log import save_log
-from train_info.epoch_log import EpochLog
-from train_info.train_log import TrainLog
-from utils import print_log
-
-
 def get_session(env, agent, learning=False, session_len=None):
     state = env.reset()
     agent.reset()
@@ -35,44 +24,11 @@ def get_session(env, agent, learning=False, session_len=None):
 
 
 def go(env, agent, episode_n=20, learning=True, show=None, session_len=None):
-    sessions = []
-
     for episode in range(episode_n):
         states, actions, rewards = get_session(env, agent, learning, session_len)
-        session = {'states': states, 'actions': actions, 'rewards': rewards}
-        sessions.append(session)
 
         if learning:
             agent.noise.reduce()
 
         if show:
-            show(env, agent, episode, [session])
-    return sessions
-
-
-def train(env, agent, epoch_n=200, episode_n=10, test_episode_n=10, log=False, log_folder='', name_suffix=''):
-    if log:
-        train_info = TrainLog(agent.name + name_suffix, agent.get_hyper_parameters())
-
-    for epoch in range(epoch_n):
-        start_time = time()
-        sessions = go(env, agent, episode_n)
-        epoch_time = time() - start_time
-
-        if log:
-            rewards = [sum(session['rewards']) for session in sessions]
-            mean_reward = np.mean(rewards)
-
-            agent.noise.turn_off()
-            test_sessions = go(env, agent, test_episode_n, False)
-            agent.noise.turn_on()
-
-            test_rewards = [sum(session['rewards']) for session in test_sessions]
-            test_mean_reward = np.mean(test_rewards)
-            std_dev = math.sqrt(np.mean([(r - test_mean_reward) ** 2 for r in test_rewards]))
-
-            epoch_info = EpochLog(epoch_time, mean_reward, rewards, test_mean_reward, test_rewards)
-            train_info.add_epoch(epoch_info)
-
-            save_log(train_info, log_folder + '\\' + train_info.name)
-            print_log(epoch, mean_reward, epoch_time, test_mean_reward, std_dev)
+            show(env, agent, episode, [{'states': states, 'actions': actions, 'rewards': rewards}])
--- main.py
+++ main.py
@@ -1,22 +1,63 @@
 import math
+from time import time
 
 import gym
+import numpy as np
 from torch import nn
 
 import DRQNERSolver
 from DRQNAgent import DRQNAgent
+from DRQNFinalHistoryAgent import DRQNFinalHistoryAgent
 from DRQNSTAgent import DRQNSTAgent
 from DRQNSTCDAgent import DRQNSTCDAgent
 from SequentialNetworkWithTypes import SequentialNetworkWithTypes, LayerType
+from log import save_log
 from other.Noises import DiscreteUniformNoise
 from other.SimpleControlProblem_Discrete import SimpleControlProblem_Discrete
 from test2 import DRQN2Agent
+from train_info.epoch_log import EpochLog
+from train_info.train_log import TrainLog
+from utils import print_log
+
+
+def add_session(sessions, session):
+    sessions += session
+
+
+def train(env, agent, epoch_n=200, episode_n=10, test_episode_n=10, log=False, log_folder='', name_suffix=''):
+    if log:
+        train_info = TrainLog(agent.name + name_suffix, agent.get_hyper_parameters())
+
+    for epoch in range(epoch_n):
+        sessions = []
+
+        start_time = time()
+        DRQNERSolver.go(env, agent, episode_n, show=lambda a, b, c, session: add_session(sessions, session))
+        epoch_time = time() - start_time
+
+        if log:
+            rewards = [sum(session['rewards']) for session in sessions]
+            mean_reward = np.mean(rewards)
+
+            agent.noise.turn_off()
+            test_sessions = DRQNERSolver.go(env, agent, test_episode_n, False)
+            agent.noise.turn_on()
+
+            test_rewards = [sum(session['rewards']) for session in test_sessions]
+            test_mean_reward = np.mean(test_rewards)
+            std_dev = math.sqrt(np.mean([(r - test_mean_reward) ** 2 for r in test_rewards]))
+
+            epoch_info = EpochLog(epoch_time, mean_reward, rewards, test_mean_reward, test_rewards)
+            train_info.add_epoch(epoch_info)
+
+            save_log(train_info, log_folder + '\\' + train_info.name)
+            print_log(epoch, mean_reward, epoch_time, test_mean_reward, std_dev)
 
 
 def main():
-    # env = gym.make("CartPole-v1")
+    env = gym.make("CartPole-v1")
     # env = DubinsCar()
-    env = SimpleControlProblem_Discrete()
+    # env = SimpleControlProblem_Discrete()
 
     state_dim = env.observation_space.shape[0]
     action_n = env.action_space.n
@@ -27,11 +68,11 @@ def main():
                                           (LayerType.Dense, 64),
                                           (LayerType.Dense, action_n)],
                                          nn.ReLU())
-    agent = DRQNAgent(state_dim, action_n, network, noise, batch_size=32, gamma=1, learning_rate=1e-2,
-                      tau=1e-2, history_len=math.inf)
+    agent = DRQNFinalHistoryAgent(state_dim, action_n, network, noise, batch_size=128, gamma=1, learning_rate=1e-2,
+                                  tau=1e-2, history_len=4)
 
-    DRQNERSolver.train(env, agent, epoch_n=1000, log=True, log_folder='logs\\SimpleControlProblem_Discrete',
-                       name_suffix='inf_test_1')
+    train(env, agent, epoch_n=1000, log=True, log_folder='logs\\CartPole',
+                       name_suffix='_test_1')
 
 
 if __name__ == '__main__':
